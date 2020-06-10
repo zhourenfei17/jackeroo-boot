0caedfd4ffd0dff2c90749d02b25432ba0a7712d
wangchengfei <zhourenfei17@126.com>
2020-06-10 20:21:41 +0800

    loading图标替换

--- jackeroo-vue/src/components/jackeroo/Spin/index.js
+++ jackeroo-vue/src/components/jackeroo/Spin/index.js
@@ -1,17 +1,38 @@
 import {Spin} from 'ant-design-vue'
 import './style.less'
 
+Spin.setDefaultIndicator({
+  indicator: {
+    render: function(h) {
+      return h('img', {
+        style :{
+          width: '150px',
+          height: '150px',
+          transform: 'translate(-50%, 0)'
+        },
+        attrs: {
+          src: require('../../../assets/loading_car.gif')
+        }
+      })
+    },
+  },
+});
+
+//全局建议使用JSpin替代Spin，这样可以统一的设置Loading的样式
 export default{
   name: 'JSpin',
   props: Object.assign({}, Spin.props, {
     wrapperClassName: {
       type: String,
       default: 'jackeroo-spin'
-    },
-    tip: {
-      type: String,
-      default: '正在努力加载中...'
     }
   }),
-  mixins: [Spin]
+  mixins: [Spin],
+  methods: {
+    /* indicator: {
+      render() {
+        return `<img src="loadImg" />`
+      },
+    }, */
+  },
 }
